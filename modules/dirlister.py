3a012237941e7d1ad4a38eda058dbd2d4ee94f9e
kali <kali@kali.raj.local>
2021-06-30 07:53:18 -0400

    Adding new modules.

---
+++ modules/dirlister.py
@@ -0,0 +1,8 @@
+import os
+
+
+def run(**args):
+    print("[*] In dirlister Module.")
+    files = os.listdir('.')
+
+    return str(files)
---
+++ modules/environment.py
@@ -0,0 +1,5 @@
+import os
+
+def run(**args):
+    print("[*] In environment Module.")
+    return os.environ
